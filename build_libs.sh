1c66470ae02832f2f4251085f93206b63098bbe0
Twarit <wtwarit@gmail.com>
2020-03-30 14:11:48 +0530

    Don't look in another dir to find libgit2.a while building

--- build_libs.sh
+++ build_libs.sh
@@ -9,7 +9,7 @@ cmake ..
 cmake --build .
 cd ../../../../
 find ./ -iname "libgit2.a" -print
-mv godot-git-plugin/thirdparty/libgit2/build/$1/libgit2.a demo/bin/x11/libgit2.a
+mv godot-git-plugin/thirdparty/libgit2/build/libgit2.a demo/bin/x11/libgit2.a
 
 cd godot-cpp/;
 apt-get install scons
